9bf6d5d0c572f534f7a5a9890c8e2cc25e1a1dfc
husam alsayed <h.alsayedahmad@simulalab.org>
2022-10-24 23:00:49 +0300

    changing heroku

--- information_retrieval_mws/settings.py
+++ information_retrieval_mws/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import django_on_heroku
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +132,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_on_heroku.settings(locals())
